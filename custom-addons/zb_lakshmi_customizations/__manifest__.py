3ada1902a92af02741c62844ce3573574b000456
jishnu <jishnuajayan.m14@gmail.com>
2025-06-30 10:42:58 +0530

    changes

--- custom-addons/zb_lakshmi_customizations/__manifest__.py
+++ custom-addons/zb_lakshmi_customizations/__manifest__.py
@@ -22,7 +22,7 @@
 {
     "name": "Lakshmi Surgicals Customizations ",
     "summary": "Lakshmi Surgicals Customizations",
-    "version": "18.0.0.28",
+    "version": "18.0.0.29",
     "category": "Accounting",
     "website": "http://www.zbeanztech.com/",
     "description": """
